ab430d6dfc15e9fbc68202533c6fb446d9833fcc
Paul Hathaway <paulchathaway@gmail.com>
2022-03-16 17:11:58 -0600

    import GLS exports and group by time-based bins

---
+++ import.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"time"
+)
+
+const GROUP_SIZE = time.Hour * 2
+
+func process(file1 string, file2 string) (datasetA map[int][]Locations, datasetB map[int][]Locations) {
+	datasetA = open(file1)
+	datasetB = open(file2)
+	return
+}
+
+func open(file string) map[int][]Locations {
+	contents, err := os.Open(file)
+	if err != nil {
+		log.Fatal("Unable to open " + fmt.Sprintf(file))
+	}
+	defer contents.Close()
+
+	bytes, err := ioutil.ReadAll(contents)
+	if err != nil {
+		log.Fatal("Unable to read " + fmt.Sprintf(file))
+	}
+
+	var locationHistory GoogleLocationHistory
+
+	err = json.Unmarshal(bytes, &locationHistory)
+	if err != nil {
+		log.Fatal("Json is invalid in " + fmt.Sprintf(file))
+	}
+
+	return categorize(locationHistory)
+
+}
+
+func categorize(locationHistory GoogleLocationHistory) map[int][]Locations {
+	groups := make(map[int][]Locations)
+
+	for _, location := range locationHistory.Locations {
+		groupId := int(location.Timestamp.Duration / GROUP_SIZE)
+		groups[groupId] = append(groups[groupId], location)
+	}
+
+	return groups
+}
