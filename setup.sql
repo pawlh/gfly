5b29e6b0aa64fbc3c54cb47397e4104a9bd7c89f
Paul Hathaway <paulchathaway@gmail.com>
2022-03-19 02:02:11 -0600

    mark most fields as non-nullable

--- setup.sql
+++ setup.sql
@@ -1,8 +1,12 @@
-CREATE TABLE locations(
-                          id INT PRIMARY KEY AUTO_INCREMENT,
-                          owner VARCHAR(255),
-                          latitude FLOAT,
-                          longitude FLOAT,
-                          accuracy INT,
-                          timestamp DATE
+DROP TABLE if exists locations;
+
+CREATE TABLE locations
+(
+    id        INT PRIMARY KEY AUTO_INCREMENT,
+    bin       INT not null,
+    owner     VARCHAR(255) not null,
+    latitude  FLOAT not null,
+    longitude FLOAT not null,
+    accuracy  INT default 0,
+    timestamp DATETIME not null
 );
