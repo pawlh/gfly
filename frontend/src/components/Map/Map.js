b34c2bea72d9e0e5d8720fa9b6b597bc66f0acdf
Paul Hathaway <paulchathaway@gmail.com>
2022-06-29 23:08:05 -0600

    render points from endpoint

--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -1,14 +1,42 @@
 import './Map.css'
-import {MapContainer, TileLayer} from 'react-leaflet'
+import {CircleMarker, MapContainer, TileLayer} from 'react-leaflet'
+import {useEffect, useState} from "react";
+
+let fetched = false
 
 export const Map = () => {
+
+    const [markerData, setMarkerData] = useState([]);
+    // const [fetched, setFetched] = useState(false)
+
+    useEffect(() => {
+        (async () => {
+            if (!fetched) {
+                fetched = true
+                const response = await fetch('http://localhost:8080/api/points')
+                setMarkerData(await response.json())
+            }
+        })()
+    }, [])
+
     return (
         <div className="Map">
-            <MapContainer id={'map'} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+            <MapContainer id={'map'} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true} preferCanvas={true} infinite={false}>
                 <TileLayer style={{filter: 'hue-rotate(45deg) !important'}}
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
+
+                {markerData?.map((point, index) => {
+                    console.log(point)
+                    return point.latitude && point.longitude ? <CircleMarker
+                        key={index}
+                        center={
+                            [point.latitude, point.longitude]
+                        }
+                        radius={10}
+                    /> : <></>
+                })}
             </MapContainer>
         </div>
     )
